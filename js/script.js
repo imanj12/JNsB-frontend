c580db6302d30c2ae2850ee8697049d43ecde120
imanj12 <imanj12@gmail.com>
2018-12-05 17:09:38 -0500

    added effects

--- js/script.js
+++ js/script.js
@@ -2,11 +2,51 @@ document.addEventListener('DOMContentLoaded', function() {
   // checkTiles()
   getChar()
   getMonster()
+  debuffsCreator()
 })
 
-let turn = true
 let char
 let monster1, monster2, monster3, monster4, monster5
+let effectsArr = []
+
+function debuffsCreator () {
+  let dysentary = document.createElement('div')
+  dysentary.id = 'dysentary'
+  dysentary.className = 'effect'
+  dysentary.innerText = 'You have dysentary lol. Your next dice roll will be halved.'
+
+  let clippers = document.createElement('div')
+  clippers.id = 'clippers'
+  clippers.className = 'effect'
+  clippers.innerText = "You're a Clippers fan. Lose a turn!"
+
+  let leftPhone = document.createElement('div')
+  leftPhone.id = 'leftphone'
+  leftPhone.className = 'effect'
+  leftPhone.innerText = "You left your phone at the start. Go back to start."
+
+  let moveBack = document.createElement('div')
+  moveBack.id = 'moveback'
+  moveBack.className = 'effect'
+  moveBack.innerText = "You're a fan of Jared Leto's joker. Move back 3 spaces."
+
+  // let index = Math.floor(Math.random() * monsterArr.length)
+  function debuffCreator(debuff, length) {
+    while (document.querySelectorAll('.effect').length < length) {
+      let boardNum = Math.floor(Math.random() * 23)
+      // debugger
+      if (document.getElementById(`board-${boardNum}`).querySelector('.effect')) {
+      } else {
+        let clone = debuff.cloneNode(true)
+        document.getElementById(`board-${boardNum}`).append(clone)
+      }
+    }
+  }
+  debuffCreator(dysentary, 2)
+  debuffCreator(clippers, 4)
+  debuffCreator(leftPhone, 6)
+  debuffCreator(moveBack, 8)
+}
 
 function getChar() {
   fetch('http://localhost:3000/characters/1')
@@ -54,6 +94,7 @@ function moveMonsters() {
   function rollAndMove(monster) {
     if (monster.children.namedItem("monster")) {
       // debugger
+      let newishTile
       if (Math.random() > .5 ) {
         let newPosition = parseInt(monster.id.split('-')[1]) + 1
         if (newPosition > 24) {
@@ -62,8 +103,8 @@ function moveMonsters() {
           newPosition = 0
         }
         let newTile = document.getElementById(`board-${newPosition}`)
-        newTile.append(monster.firstElementChild)
-        return newTile
+        // newTile.append(monster.firstElementChild)
+        newishTile = newTile
       } else {
         let newPosition = parseInt(monster.id.split('-')[1]) - 1
         if (newPosition > 24) {
@@ -72,36 +113,38 @@ function moveMonsters() {
           newPosition = 0
         }
         let newTile = document.getElementById(`board-${newPosition}`)
-
-        if (newTile.children.namedItem("player")) {
-          let captiveMonster = Array.from(document.getElementById('sprites-container').children).find(function(child) {
-            return child.innerHTML != ''
-          })
-          if (captiveMonster) {
-            captiveMonster.innerHTML = ''
-          } else {
-            alert('Game over!')
-          }
-        } else {
-          newTile.append(monster.firstElementChild)
-        }
-        return newTile
+        // newTile.append(monster.firstElementChild)
+        newishTile = newTile
       }
+      if (newishTile.children.namedItem("player")) {
+        let captiveMonster = Array.from(document.getElementById('sprites-container').children).find(function(child) {
+          return child.innerHTML != ''
+        })
+        if (captiveMonster) {
+          captiveMonster.innerHTML = ''
+          // debugger
+          monster.children.namedItem("monster").remove()
+        } else {
+          alert('Game over!')
+        }
+      } else {
+        newishTile.append(monster.children.namedItem('monster'))
+      }
+      return newishTile
     }
 
-
-
   }
-  setTimeout(() => {monster1 = rollAndMove(monster1)}, 500)
-  setTimeout(() => {monster2 = rollAndMove(monster2)}, 1000)
-  setTimeout(() => {monster3 = rollAndMove(monster3)}, 2000)
-  setTimeout(() => {monster4 = rollAndMove(monster4)}, 3000)
+  setTimeout(() => {monster1 = rollAndMove(monster1)}, 250)
+  setTimeout(() => {monster2 = rollAndMove(monster2)}, 500)
+  setTimeout(() => {monster3 = rollAndMove(monster3)}, 1000)
+  setTimeout(() => {monster4 = rollAndMove(monster4)}, 1500)
   setTimeout(() => {
     monster5 = rollAndMove(monster5)
+    // debugger
     document.getElementById('rollBtn').disabled = false
     document.getElementById('turn-character').style.background = 'green'
     document.getElementById('turn-monster').style.background = 'white'
-  }, 4000)
+  }, 2000)
 
 }
 
@@ -123,7 +166,7 @@ function rollDiceWithoutValues() {
 
 function moveChar (value) {
   let currentPosition = document.getElementById("player").parentElement
-  let newPosition = parseInt(currentPosition.id.split("-")[1]) + 1 // REMEMBER!!!!!!!!!!!!!!!!!!!!
+  let newPosition = parseInt(currentPosition.id.split("-")[1]) + 2 // REMEMBER!!!!!!!!!!!!!!!!!!!!
   if (newPosition > 24) {
     newPosition = 24
   }
@@ -133,8 +176,7 @@ function moveChar (value) {
   if (newTile.children.namedItem('monster')) {
     let spritesContainer = document.getElementById('sprites-container')
     let i = 0
-    while (newTile.children.length > 0) {
-      // debugger
+    while (!!newTile.children.namedItem('monster')) {
       if (spritesContainer.children[i].innerHTML === '') {
         spritesContainer.children[i].append(newTile.children.namedItem('monster'))
         // reset monster 1
