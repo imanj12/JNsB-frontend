c27cc21351a90764f8f643d89d739bbc683c2d8d
imanj12 <imanj12@gmail.com>
2018-12-05 11:50:11 -0500

    Dec 5 Lunch Commit

--- js/script.js
+++ js/script.js
@@ -27,25 +27,66 @@ function getMonster () {
 function start(char) {
   let firstTile = document.getElementById('board-0')
   firstTile.append(insert(char, "player"))
+  document.getElementById('turn-character').style.background = 'green'
 }
 
 function setMonster (monsters) {
   let selectedMonsters = monsterPicker(monsters)
     // debugger
-  let firstMonster = document.getElementById('board-2')
-  firstMonster.append(insert(selectedMonsters[0], "monster"))
+  monster1 = document.getElementById('board-2')
+  monster1.append(insert(selectedMonsters[0], "monster"))
 
-  let secondMonster = document.getElementById('board-7')
-  secondMonster.append(insert(selectedMonsters[1], "monster"))
+  monster2 = document.getElementById('board-7')
+  monster2.append(insert(selectedMonsters[1], "monster"))
 
-  let thirdMonster = document.getElementById('board-12')
-  thirdMonster.append(insert(selectedMonsters[2], "monster"))
+  monster3 = document.getElementById('board-12')
+  monster3.append(insert(selectedMonsters[2], "monster"))
 
-  let fourthMonster = document.getElementById('board-17')
-  fourthMonster.append(insert(selectedMonsters[3], "monster"))
+  monster4 = document.getElementById('board-17')
+  monster4.append(insert(selectedMonsters[3], "monster"))
 
-  let fifthMonster = document.getElementById('board-22')
-  fifthMonster.append(insert(selectedMonsters[4], "monster"))
+  monster5 = document.getElementById('board-22')
+  monster5.append(insert(selectedMonsters[4], "monster"))
+}
+
+function moveMonsters() {
+
+  function rollAndMove(monster) {
+    if (monster.children.length > 0) {
+      if (Math.random() > .5 ) {
+        let newPosition = parseInt(monster.id.split('-')[1]) + 1
+        if (newPosition > 24) {
+          newPosition = 24
+        } else if (newPosition < 0) {
+          newPosition = 0
+        }
+        let newTile = document.getElementById(`board-${newPosition}`)
+        newTile.append(monster.firstElementChild)
+        return newTile
+      } else {
+        let newPosition = parseInt(monster.id.split('-')[1]) - 1
+        if (newPosition > 24) {
+          newPosition = 24
+        } else if (newPosition < 0) {
+          newPosition = 0
+        }
+        let newTile = document.getElementById(`board-${newPosition}`)
+        newTile.append(monster.firstElementChild)
+        return newTile
+      }
+    }
+  }
+  setTimeout(() => {monster1 = rollAndMove(monster1)}, 500)
+  setTimeout(() => {monster2 = rollAndMove(monster2)}, 1000)
+  setTimeout(() => {monster3 = rollAndMove(monster3)}, 2000)
+  setTimeout(() => {monster4 = rollAndMove(monster4)}, 3000)
+  setTimeout(() => {
+    monster5 = rollAndMove(monster5)
+    document.getElementById('rollBtn').disabled = false
+    document.getElementById('turn-character').style.background = 'green'
+    document.getElementById('turn-monster').style.background = 'white'
+  }, 4000)
+
 }
 
 function checkTiles () {
@@ -66,13 +107,36 @@ function rollDiceWithoutValues() {
 
 function moveChar (value) {
   let currentPosition = document.getElementById("player").parentElement
-  let newPosition = parseInt(currentPosition.id.split("-")[1]) + parseInt(value)
+  let newPosition = parseInt(currentPosition.id.split("-")[1]) + 2 // REMEMBER!!!!!!!!!!!!!!!!!!!!
+  if (newPosition > 24) {
+    newPosition = 24
+  }
   let newTile = document.getElementById(`board-${newPosition}`)
+  // check if newTile has child element of monster
+  // if newTile has monster
+    // then remove monster and put in sprites div
+
+
+  if (newTile.children.namedItem('monster')) {
+    let spritesContainer = document.getElementById('sprites-container')
+    let i = 0
+    while (newTile.children.length > 0) {
+      // debugger
+      if (spritesContainer.children[i].innerHTML === '') {
+        spritesContainer.children[i].append(newTile.children.namedItem('monster'))
+        // reset monster 1
+      }
+      i++
+    }
+  }
+
+
   newTile.append(insert(char, "player"))
-  removeChar(currentPosition)
-  // disable roll
-  // highlight monster turn
-  // function to move monsters
+  removeChar(currentPosition, 'player')
+  document.getElementById('rollBtn').disabled = true
+  document.getElementById('turn-character').style.background = 'white'
+  document.getElementById('turn-monster').style.background = 'green'
+  moveMonsters()
     // at end of this function, re-enable roll, highlight character turn
 }
 
@@ -86,8 +150,8 @@ function insert (char, id) {
   return img
 }
 
-function removeChar (element) {
-  element.children.namedItem('player').remove()
+function removeChar (element, id) {
+  element.children.namedItem(id).remove()
 }
 
 function monsterPicker (monsterArr) {
